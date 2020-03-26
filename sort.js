819f0f35d20369e6f83339b4f06601b45de79798
kishikyousuke <carp011808pg@icloud.com>
2020-03-27 02:51:10 +0900

    配列をソートする処理を追加

--- sort.js
+++ sort.js
@@ -1,2 +1,12 @@
+var sortNumber = function (nunber) {
+	number.sort(function (a,b) {
+		ig (a === b) {
+			return 0;
+		}
+		return a < b ? -1 : 1;
+	});
+};
+
 var number = [19, 3, 81, 1, 2 , 21];
+sortNumber(number);)
 console.log(number);
